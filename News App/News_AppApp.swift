4545131f6a96c14a1303fb669d94fbef9b360db6
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2022-11-15 20:48:28 +0530

    project files added

---
+++ News App/News_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_AppApp.swift
+//  News App
+//
+//  Created by Hansa Anuradha on 2022-11-15.
+//
+
+import SwiftUI
+
+@main
+struct News_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
